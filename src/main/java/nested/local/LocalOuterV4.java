b069797b5f6453c4d2e2a7f8654f9be241620b71
eunbikim <eunbikim@autoeveramerica.com>
2024-11-27 14:20:42 +0900

    8. 중첩 클래스, 내부 클래스

--- src/main/java/nested/local/LocalOuterV4.java
+++ src/main/java/nested/local/LocalOuterV4.java
@@ -1,14 +1,13 @@
 package nested.local;
 
-import java.lang.reflect.Field;
-
-public class LocalOuterV3 {
+public class LocalOuterV4 {
     private int outInstanceVar = 3;
 
     public Printer process(int paramVar) {
-        int localVar = 1; // 지역 변수는 스택 프레임이 종료되는 순간 함께 제거된다.
 
-        class LocalPrinter implements Printer{
+        int localVar = 1;
+
+        class LocalPrinter implements Printer {
             int value = 0;
 
             @Override
@@ -24,22 +23,16 @@ public class LocalOuterV3 {
         }
 
         Printer printer = new LocalPrinter();
-        //printer.print() 를 여기서 실행하지 않고 Printer 인스턴스만 반환한다.
+        //만약 localVar의 값을 변경한다면? 다시 캡처해야 하나??
+        // localVar = 10; // 컴파일 오류
+        // paramVar = 20; // 컴파일 오류
         return printer;
-
     }
 
     public static void main(String[] args) {
-        LocalOuterV3 localOuter = new LocalOuterV3();
+        LocalOuterV4 localOuter = new LocalOuterV4();
         Printer printer = localOuter.process(2);
-        //printer.print() 를 나중에 실행한다. process() 의 스택 프레임이 사라진 이후에 실행
         printer.print();
-
-        // 추가
-        System.out.println("필드 확인");
-        Field[] fields = printer.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            System.out.println("field = " + field);
-        }
     }
+
 }
